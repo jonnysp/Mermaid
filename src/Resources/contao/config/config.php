8a40865598ce1180c32ef8fc1b00b518ae96edd7
jonnysp <jonnyspitzner@googlemail.com>
2023-05-03 13:25:12 +0200

    changes

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,23 +9,10 @@
  */
 
 /**
- * Back end modules
- */
-//array_insert($GLOBALS['BE_MOD']['pannorama'], 100, array
-//(
-//	'pannorama' => array
-//	(
-//		'tables' => array('tl_pannorama', 'tl_pannorama_scene','tl_pannorama_hotspot')
-//	)
-//));
-//
-
-/**
  * Style sheet
  */
 if (TL_MODE == 'BE')
 {
-	//$GLOBALS['TL_CSS'][] = 'bundles/jonnysppannorama/pannorama.css|static';
 	$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/jonnyspmermaid/mermaid.min.js';
 }
 
@@ -40,14 +27,4 @@ array_insert($GLOBALS['TL_CTE'], 1, array
 		'mermaid_viewer'    => 'MermaidViewer'
 	)
 ));
-
-/**
- * Back end form fields
- */
-//  array_insert($GLOBALS['BE_FFL'] ,1, array
-//  (
-//  	'pannoramasceneposition'        => 'PannoramaScenePositionSelector',
-//  	'pannoramahotspotposition'      => 'PannoramaHotspotPositionSelector',
-//  	'pannoramatargetposition'		=> 'PannoramaTargetPositionSelector'
-//  ));
 
--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,40 +1,10 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['mermaid_viewer'] = '{type_legend},type;{mermaid_legend},code;{expert_legend:hide},cssID,space';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['mermaid_viewer'] = '{type_legend},type;{mermaid_legend},mermaid;{expert_legend:hide},cssID,space';
 
-//$GLOBALS['TL_DCA']['tl_content']['fields']['pannoramaviewer'] = array
-//(
-//	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['pannorama_viewer'],
-//	'inputType'               => 'select',
-//	'options_callback'        => array('tl_content_pannorama', 'getPannorama'),
-//	'eval'                    => array('mandatory'=>true, 'chosen'=>true, 'submitOnChange'=>true),
-//	'wizard' 				  => array(array('tl_content_pannorama', 'editPannorama')),
-//	'sql'                     => "int(10) unsigned NOT NULL default '0'"
-//);
-
-
-//class tl_content_pannorama extends Backend 
-//{
-//
-//	public function getPannorama()
-//	{
-//		$objCats =  \PannoramaModel::findAll();
-//		$arrCats = array();
-//		if (isset($objCats)) {
-//			foreach ($objCats as $objCat)
-//			{
-//				$arrCats[$objCat->id] =  $objCat->title .' (ID ' . $objCat->id . ')';
-//			}
-//		}
-//		return $arrCats;
-//	}
-//
-//	public function editPannorama(DataContainer $dc)
-//	{
-//		$this->loadLanguageFile('tl_pannorama');
-//		return ($dc->value < 1) ? '' : ' <a href="contao/main.php?do=pannorama&amp;act=edit&amp;id=' . $dc->value . '&amp;popup=1&amp;nb=1&amp;rt=' . REQUEST_TOKEN . '" title="' . sprintf(StringUtil::specialchars($GLOBALS['TL_LANG']['tl_pannorama']['edit'][1]), $dc->value) . '" onclick="Backend.openModalIframe({\'title\':\'' . StringUtil::specialchars(str_replace("'", "\\'", sprintf($GLOBALS['TL_LANG']['tl_pannorama']['edit'][1], $dc->value))) . '\',\'url\':this.href});return false">' . Image::getHtml('alias.svg', $GLOBALS['TL_LANG']['tl_pannorama']['edit'][0]) . '</a>';
-//	}
-//
-//
-//}
-//
+$GLOBALS['TL_DCA']['tl_content']['fields']['mermaid'] = array (
+    'label'                   => &$GLOBALS['TL_LANG']['tl_content']['mermaid_viewer'],
+    'inputType'               => 'textarea',
+    'eval'                    => array('mandatory'=>true, 'preserveTags'=>true, 'decodeEntities'=>true, 'class'=>'monospace', 'rte'=>'ace', 'helpwizard'=>false, 'tl_class'=>'clr'),
+    'sql'                     => "text NULL"
+);
