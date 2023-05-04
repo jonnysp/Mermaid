3a574c5c77e66b451d98ef533faf9a14cf64dfd4
jonnysp <jonnyspitzner@googlemail.com>
2023-05-04 12:12:00 +0200

    fix lang

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['CTE']['mermaid_viewer'] = "Mermaid Diagramm";
+$GLOBALS['TL_LANG']['CTE']['mermaid_viewer'] = array("Mermaid Diagramm","Mermaid Diagramm");
 
 $GLOBALS['TL_LANG']['tl_content']['mermaid_viewer'] = array("Mermaid Diagramm",'Hilfe  <a href="https://mermaid.js.org" target="_blank">https://mermaid.js.org</a>');
 $GLOBALS['TL_LANG']['tl_content']['mermaid_legend'] = "Mermaid Diagramm";
